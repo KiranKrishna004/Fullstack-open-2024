0ccdd33a07c569f25b632ea8b5b063ce379e7fa6
KiranKrishna004 <kiran.004narath@gmail.com>
2024-07-16 00:10:38 +0530

    refactor: change text listing to table listing

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,16 +4,35 @@ const Button = ({ handleFeedback, feedback }) => {
   return <button onClick={handleFeedback}>{feedback}</button>;
 };
 
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
 const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
-      <h2>statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good + neutral + bad}</p>
-      <p>average: {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
-      <p>positive: {good / (good + neutral + bad)}</p>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <tr>
+            <td>all</td>
+            <td>{good + neutral + bad}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{(good * 1 + bad * -1) / (good + neutral + bad)}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td> {(good / (good + neutral + bad)) * 100}%</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
@@ -34,7 +53,12 @@ const App = () => {
       />
       <Button feedback={"bad"} handleFeedback={() => setBad(bad + 1)} />
 
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h2>statistics</h2>
+      {bad + good + neutral === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
   );
 };
