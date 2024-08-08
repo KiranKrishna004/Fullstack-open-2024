7b71a9ca61b60eb20e46cd12bf1c34b3be15d114
KiranKrishna004 <kiran.004narath@gmail.com>
2024-08-09 01:01:32 +0530

    feat: add linting and remove mongo.js

--- part2/phonebook/backend/models/person.js
+++ part2/phonebook/backend/models/person.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 mongoose
   .connect(url)
 
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 3 },
@@ -26,18 +26,18 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{7,8}/.test(v);
+        return /\d{2,3}-\d{7,8}/.test(v)
       },
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
